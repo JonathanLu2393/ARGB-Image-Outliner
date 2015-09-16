ff01985464f632eab3493c464eaefdbae7b3cbcb
Jonathan Lu <jonathanlu2393@gmail.com>
2015-09-16 16:40:24 -0700

    All done! :)
    (Note: Assumes user will enter numbers between 0-255)

--- Outline.java
+++ Outline.java
@@ -3,6 +3,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.awt.Color;
+import java.util.Scanner;
 
 public class outline {
 
@@ -16,38 +17,43 @@ public class outline {
 		if( !file.exists() ) {
 			System.exit(0);
 		}	
-	
-		//Creating the border color
-		int a = 0;
-		int r = 0;
-		int g = 0;
-		int b = 0;
+		
+		//Takes user input, inputs an INT
+		Scanner user_input = new Scanner( System.in );
+		System.out.print("Please enter Alpha value (0-255): ");
+		int a = user_input.nextInt();
+		
+		System.out.print("Please enter Red value (0-255)  : ");
+		int r = user_input.nextInt();
+		
+		System.out.print("Please enter Green value (0-255): ");
+		int g = user_input.nextInt();
+		
+		System.out.print("Please enter Blue value (0-255) : ");
+		int b = user_input.nextInt();
+
+		//Sets the border color
 		Color border = new Color(r,g,b,a);
 	
 		//Height and Width of the Image
 		int w = image.getWidth();
 		int h = image.getHeight();
-		System.out.println("width, height: " + w + ", " + h);
 	
+		//loops through all the pixels
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
-				int pixel = image.getRGB(j, i);
-				int alpha = (pixel >> 24) & 0xff;
-				int red = (pixel >> 16) & 0xff;
-				int green = (pixel >> 8) & 0xff;
-				int blue = (pixel) & 0xff;
+
+				//finds the border pixels
 				if(i == 0 || i == h-1 || j == 0 || j == w-1)
 				{
-					//System.out.println("x,y: " + j + ", " + i);
-					//System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
+					//sets the border pixels to desired color
 					image.setRGB(j,i,border.getRGB());
 				}
 			}
 		}
 		
 		File output = new File("output.png");
-		ImageIO.write(image, "png", output);
-		
+		ImageIO.write(image, "png", output);	
 	}
 }
 
