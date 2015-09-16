f1613fb1897b55d973f5b9af771243a4d54adaad
Jonathan Lu <jonathanlu2393@gmail.com>
2015-09-16 15:53:15 -0700

    Gives the value and coordinates to each pixel.

--- Outline.java
+++ Outline.java
@@ -21,19 +21,18 @@ public class Outline {
 		int h = image.getHeight();
 		System.out.println("width, height: " + w + ", " + h);
 	
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				System.out.println("x,y: " + j + ", " + i);
+				int pixel = image.getRGB(j, i);
+				int alpha = (pixel >> 24) & 0xff;
+				int red = (pixel >> 16) & 0xff;
+				int green = (pixel >> 8) & 0xff;
+				int blue = (pixel) & 0xff;
+				System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
+			}
+		}
 	}
-	
-	
-	
-	public void printPixelARGB(int pixel) {
-		int alpha = (pixel >> 24) & 0xff;
-		int red = (pixel >> 16) & 0xff;
-		int green = (pixel >> 8) & 0xff;
-		int blue = (pixel) & 0xff;
-		System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
-	}
-	
-	
 }
 
 
