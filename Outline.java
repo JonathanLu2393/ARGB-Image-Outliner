5a19c3e9c68b169c2d0707a9ab5f806c5e616235
Jonathan Lu <jonathanlu2393@gmail.com>
2015-09-17 14:17:12 -0700

    Renamed variables, reordered some functions, cleaned up border check

--- Outline.java
+++ Outline.java
@@ -10,13 +10,19 @@ public class outline {
 	public static void main(String [] args) throws Exception{
 		
 		//Input image, has to be in ARGB format
-		File file = new File("test.png");
-		BufferedImage image =  ImageIO.read(file);
-		
+		File file = new File("test.png");	
 		//Check if file exists
 		if( !file.exists() ) {
 			System.exit(0);
 		}	
+		
+		//Read in the file
+		BufferedImage image =  ImageIO.read(file);
+		
+		//Height and Width of the Image
+		int imageWidth = image.getWidth();
+		int imageHeight = image.getHeight();
+		int[][] border_pixels = new int[imageWidth][imageHeight];
 		
 		//Takes user input, inputs an INT
 		Scanner user_input = new Scanner( System.in );
@@ -35,59 +41,57 @@ public class outline {
 		//Sets the border color
 		Color border = new Color(r,g,b,a);
 	
-		//Height and Width of the Image
-		int w = image.getWidth();
-		int h = image.getHeight();
-		
-		int[][] border_pixels = new int[w][h];
-	
 		//loops through all the pixels
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if(i == 0) {
-					if(image.getRGB(j,i) != 0) {
-						//sets the border pixels to desired color
-						image.setRGB(j,i,border.getRGB());
-					}
-				}
+		for (int i = 0; i < imageHeight; i++) {
+			for (int j = 0; j < imageWidth; j++) {
 				
-				if(i == h-1) {
-					if(image.getRGB(j,i) != 0) {
+				if(isImageEdgePixel(j,i,imageHeight, imageWidth) && (image.getRGB(j,i) != 0)) {
 						//sets the border pixels to desired color
 						image.setRGB(j,i,border.getRGB());
-					}
-				}
-				
-				if(j == 0) {
-					if(image.getRGB(j,i) != 0) {
-						//sets the border pixels to desired color
-						image.setRGB(j,i,border.getRGB());
-					}
-				}
-				
-				if(j == w-1) {
-					if(image.getRGB(j,i) != 0) {
-						//sets the border pixels to desired color
-						image.setRGB(j,i,border.getRGB());
-					}
 				}
 
-				else if(i > 0 && i < h-1 && j > 0 && j < w-1){
+				if(i > 0 && i < imageHeight-1 && j > 0 && j < imageWidth-1){
 					int before = image.getRGB(j-1,i);
 					int after = image.getRGB(j+1,i);
 					int above = image.getRGB(j,i+1);
 					int under = image.getRGB(j,i-1);
 					
 					if(before == 0 || after == 0 || above == 0 || under == 0) {
-						
 						if(image.getRGB(j,i) != 0) {
 							//sets the border pixels to desired color
 							border_pixels[j][i] = 1;
 						}
 					}
 				}
-			
-			
+			}
+		}
+		
+		for (int i = 0; i < imageHeight; i++) {
+			for (int j = 0; j < imageWidth; j++) {
+				if(border_pixels[j][i] == 1) {
+					//System.out.println(image.getRGB(j,i));
+					image.setRGB(j,i,border.getRGB());
+				}
+			}
+		}
+		
+		File output = new File("output.png");
+		ImageIO.write(image, "png", output);	
+	}
+	
+	public static boolean isImageEdgePixel(int j, int i, int h, int w) {
+		if(i == 0) {return true;}
+		else if(i == h-1) {return true;}
+		else if(j == 0) {return true;}
+		else if(j == w-1) {return true;}
+		else {return false;}
+	}
+	
+	
+	
+}
+
+
 			/*  Prints out ARGB values of all pixels */ /*
 				int pixel = image.getRGB(j, i);
  				int alpha = (pixel >> 24) & 0xff;
@@ -97,22 +101,3 @@ public class outline {
 				System.out.println("x,y: " + j + ", " + i);
 				System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
 			*/
-			}
-		}
-		
-		
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if(border_pixels[j][i] == 1) {
-					image.setRGB(j,i,border.getRGB());
-				}
-			}
-		}
-		
-		
-		File output = new File("output.png");
-		ImageIO.write(image, "png", output);	
-	}
-}
-
-
